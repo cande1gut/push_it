3ae2ce4c9d457d0ccf0f53045eb4256f7b644e59
cande1gut <candelario.gtz@gmail.com>
2022-12-30 15:48:06 -0700

    rewrite

--- js/functions.js
+++ js/functions.js
@@ -2,29 +2,30 @@ $( document ).ready(function() {
 
 	var click = new Audio("sounds/ButtonTap.wav");
 	var time_ch = new Audio("sounds/SecondBeep.wav");
-	var song = new Audio("sounds/HallOfTheMountainKing.mp3");
 	var score = 0;
 	var c = 30;
 	var wait = 2;
 	var interval = setInterval(tiempo, 1000);
 
 	function tiempo(){
-		song.play();
 		c--;
 		$("#timer").text(c);
 		time_ch.play();
 		if(c == 0)
 		{
 			clearInterval(interval);
-			$("#number2").text("Congratulations! you tapped the button " + score + " times");
-			$('#myModal').modal('toggle');
+			alert("Congratulations! you tapped the button " + score + " times");
 			wait--;
 			if(wait == 0){
-				song.pause();
-				song.currentTime = 0
 				time_ch.pause();
 				time_ch.currentTime = 0
 			}
+			score = 0;
+			c = 30;
+			wait = 2;
+			$("#number").text(score);
+			$("#timer").text(30);
+			interval = setInterval(tiempo, 1000);
 		}
 	}
 
@@ -40,15 +41,5 @@ $( document ).ready(function() {
 		score++;
 		$("#number").text(score);
 		click.play();
-	});
-
-	$("#boton_re").on("click touch",function () {
-		$('#myModal').modal('toggle');
-		score = 0;
-		c = 30;
-		wait = 2;
-		$("#number").text(score);
-		$("#timer").text(30);
-		interval = setInterval(tiempo, 1000);
 	});
 });
